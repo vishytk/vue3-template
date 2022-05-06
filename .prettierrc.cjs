2673f48f1f3b17d7344280462f4fd50ad3000445
Vishwanath T K <vishwanath.tk@gmail.com>
2022-05-06 08:23:31 +0530

    add prettier configuration

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  printWidth: 80,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+}
