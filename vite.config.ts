ac1fd520ac1249a61b56521759658fa6d2f7f06b
Vishwanath T K <vishwanath.tk@gmail.com>
2022-05-28 09:27:51 +0530

    add and configure 'unplugin-vue-components' plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,21 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
+import Components from 'unplugin-vue-components/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(), Pages(), Layouts()],
+  plugins: [
+    Vue(),
+    Pages(),
+    Layouts(),
+    Components({
+      // support vue and markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // auto import and register vue and markdown components
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': `${path.resolve(__dirname, 'src')}/`,
